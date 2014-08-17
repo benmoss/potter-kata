cbeb926f3f3dcf0fdbeec87702e7696ce36aeca7
Ben Moss <ben@mossity.com>
2014-08-16 20:04:02 -0400

    Sorta works

--- src/kata_potter/core.clj
+++ src/kata_potter/core.clj
@@ -1,5 +1,49 @@
 (ns kata-potter.core)
 
+(def discount-for-set
+  {5 0.75
+   4 0.80
+   3 0.90
+   2 0.95
+   1 1.00})
+
+(defn possible-totals [book-freqs]
+  (into #{}
+        (for [set-size [1 2 3 4 5]
+              :let [distinct-books-count (count book-freqs)]
+              :when (>= distinct-books-count set-size)]
+          (loop [book-freqs book-freqs
+                 total 0]
+            (if (not-any? (fn [[k,v]] (zero? v)) book-freqs)
+              (recur (into {} (map (fn [[k,v]] [k (dec v)]) book-freqs))
+                     (+ total (* (discount-for-set set-size)
+                                 distinct-books-count
+                                 8)))
+              total)))))
+
+
+(declare total discount)
+
+(defn discount-groups
+  ([books] (discount-groups books [] 5))
+  ([books totals set-size]
+   (let [freqs (frequencies books)]
+     (if (> set-size (count freqs))
+       (recur books totals (dec set-size))
+       (recur books (conj totals (total books set-size)) (dec set-size))))))
+
+
+
+; 2 2 1
+; buy 5 separately, 5*8 = 40
+; buy one set of 3, one set of 2, 3*8*0.90 + 2*8*0.95 0 =  36.8
+; buy two sets of 2, one by itself 4*8*0.95 + 8 = 38.4
+
+; two of same, one other
+; 2 at 0.95, one at 1.0
 
 (defn cost [books]
-  (* 8 (count books)))
+  (let [freqs (frequencies books)
+        disc (discount (count freqs))]
+    (reduce (fn [total [book number-of-books]]
+              (* discount )))))
--- test/kata_potter/core_test.clj
+++ test/kata_potter/core_test.clj
@@ -1,11 +1,32 @@
 (ns kata-potter.core-test
-  (:require [clojure.test.check :as tc]
+  (:require [clojure.test :refer :all]
+            [clojure.test.check :as tc]
             [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :as prop]
-            [kata-potter.core :refer :all]))
+            [clojure.test.check.clojure-test :refer [defspec]]
+            [kata-potter.core :as kata]))
 
-(def uniq-books-cost-8
-  (prop/for-all [books (gen/fmap set (gen/vector gen/string))]
-                (= (cost books) (* 8 (count books)))))
+; (deftest a-test
+  ; (testing "FIXME, I fail."
+    ; (is (= 0 1))))
 
-(tc/quick-check 100 uniq-books-cost-8)
+(deftest cost
+  (testing "a book costs 8"
+    (is (= (kata/cost ["a book"]) 8)))
+  (testing "two of the same book cost 16"
+    (is (= (kata/cost ["a book" "a book"]) 16))))
+
+(deftest possible-totals
+  (is (= (kata/possible-totals {1 2}) #{16.0}))
+  (is (= (kata/possible-totals {1 1, 2 1}) #{15.20, 16.0}))
+  (is (= (kata/possible-totals {1 2, 2 2}) #{32.0, 30.40}))
+  (is (= (kata/possible-totals {1 1, 2 1, 3 1, 4 1}) #{32.0 30.4 29.6 25.6})))
+
+(defn no-dups? [coll]
+  (= (-> coll set count)
+     (count coll)))
+
+(defspec two-distinct-books-discount
+  100
+  (prop/for-all [books (gen/such-that no-dups? (gen/vector gen/int) 100)]
+                (= (kata/cost books) 15.20)))
